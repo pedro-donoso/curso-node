67013bf7705610110e5857d92a66932045bae75b
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-02 17:27:09 -0300

    emitir eventos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const EventEmitter = require('events');
 const emisorProductos = new EventEmitter();
-emisorProductos.on('compra', () => {
-    console.log('Se realizo una compra');
+emisorProductos.on('compra', (numProductos, total) => {
+    console.log(`Se realizo una compra por la cantidad de: ${numProductos} productos`);
+    console.log(`Por un precio total de: ${total} pesos`);
 });
-emisorProductos.emit('compra');
+emisorProductos.emit('compra', 8, 5000);
