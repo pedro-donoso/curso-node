4e367d3aa75469dd8347afc1bf3722abe9aa5ba1
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-03 10:49:44 -0300

    confirmacion

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-const EventEmitter = require('events');
+/* const EventEmitter = require('events');
 const emisorProductos = new EventEmitter();
 emisorProductos.on('compra', (numProductos, total) => {
     console.log(`Se realizo una compra por la cantidad de: ${numProductos} productos`);
     console.log(`Por un precio total de: ${total} pesos`);
 });
-emisorProductos.emit('compra', 8, 5000);
+emisorProductos.emit('compra', 8, 5000); */
+
+// ejemplo
+
+/* const promesaCumplida = true;
+const miPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (promesaCumplida) {
+            resolve('¡Promesa cumplida!');
+        } else {
+            reject('Promesa rechazada...');
+        }
+    }, 3000);
+});
+
+const manejarPromesaCumplida = (valor) => {
+    console.log(valor);
+}
+const manejarPromesaRechazada = (razonRechazo) => {
+    console.log(razonRechazo);
+}
+miPromesa.then(manejarPromesaCumplida, manejarPromesaRechazada); */
+
+// pizzas
+
+const estatusPedido = () => {
+    return Math.random() < 0.8;
+};
+
+const miPedidoDePizza = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (estatusPedido()) {
+            resolve('¡Pedido exitoso! Su pizza está en camino');
+        } else {
+            reject('Ocurrió un error. Por favor intente nuevamente');
+        }
+    }, 3000);
+});
+
+miPedidoDePizza
+    .then((mensajeDeConfirmacion) => {
+        console.log(mensajeDeConfirmacion);
+    })
+    .then(null, (mensajeDeError) => {
+        console.log(mensajeDeError);
+    });
+
