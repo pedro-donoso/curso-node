2240e1aec234b4aa9d1914df8b842875ef69a5a7
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 14:30:40 -0300

    matematicas

--- curso-express/index.js
+++ curso-express/index.js
@@ -16,7 +16,7 @@ app.get('/api/cursos/programacion', (req, res) => {
     res.send(JSON.stringify(infoCursos.programacion))
 })
 
-// parametros url
+// parametros url programacion
 app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje;
     const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
@@ -30,6 +30,16 @@ app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
 app.get('/api/cursos/matematicas', (req, res) => {
     res.send(JSON.stringify(infoCursos.matematicas))
 }) 
+// parametros url matematicas
+app.get('/api/cursos/matematicas/:tema', (req, res) => {
+    const tema = req.params.tema;
+    const resultados = infoCursos.matematicas.filter(curso => curso.tema === tema);
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${tema}`);
+    }
+    res.send(JSON.stringify(resultados));
+});
+
 
 const PUERTO = process.env.PORT || 3000;
 app.listen(PUERTO, () => {
