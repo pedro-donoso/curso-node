2ac547393a25fc108d76846780c0aacd50d69a18
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 13:39:51 -0300

    arreglo json

--- curso-express/index.js
+++ curso-express/index.js
@@ -3,10 +3,19 @@ const app = express();
 // desestructuracion infocursos, para importarlo directamente
 const { infoCursos } = require('./cursos-bd');
 console.log(infoCursos);
-// routing
+// routing sitio principal
 app.get('/', (req, res) => {
     res.send('Mi primer servidor con Express. Cursos');
 });
+// routing cursos
+app.get('/api/cursos', (req, res) => {
+    res.send(JSON.stringify(infoCursos));
+});
+// routing curso programacion
+app.get('/api/cursos/programacion', (req, res) => {
+    res.send(JSON.stringify(infoCursos.programacion))
+})
+
 const PUERTO = process.env.PORT || 3000;
 app.listen(PUERTO, () => {
     console.log(`El servidor esta escuchando en el puerto ${PUERTO}`);
