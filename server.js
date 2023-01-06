9a4c9c4472e26cd3a6e292b547a692beda6c6dd0
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 10:47:08 -0300

    server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 const servidor = http.createServer((req, res) => {
-    res.end('Hola mundo');
+    res.end('Probando servidor');
 });
 const PUERTO = 3000;
 servidor.listen(PUERTO, () => {
