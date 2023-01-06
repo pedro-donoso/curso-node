d34ae78519a0f566be384baf0c8b1185a9eb214d
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 14:21:07 -0300

    busqueda avanzada

--- curso-express/index.js
+++ curso-express/index.js
@@ -21,9 +21,10 @@ app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
     const lenguaje = req.params.lenguaje;
     const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje);
     if (resultados.length === 0) {
-        return res.send(`No se encontraron cursos de ${lenguaje}`);
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje}`);
     }
-})
+    res.send(JSON.stringify(resultados));
+});
 
 // routing cursos de matematicas
 app.get('/api/cursos/matematicas', (req, res) => {
