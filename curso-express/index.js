ba8c9b21b14c98817d57f5c53c7cf19d5560c8c3
pedro donoso ramirez <pedro.donoso.r@usach.cl>
2023-01-06 14:41:09 -0300

    niveles

--- curso-express/index.js
+++ curso-express/index.js
@@ -26,6 +26,16 @@ app.get('/api/cursos/programacion/:lenguaje', (req, res) => {
     res.send(JSON.stringify(resultados));
 });
 
+app.get('/api/cursos/programacion/:lenguaje/:nivel', (req, res) => {
+    const lenguaje = req.params.lenguaje;
+    const nivel = req.params.nivel;
+    const resultados = infoCursos.programacion.filter(curso => curso.lenguaje === lenguaje && curso.nivel === nivel);
+    if (resultados.length === 0) {
+        return res.status(404).send(`No se encontraron cursos de ${lenguaje} de nivel ${nivel}`)
+    }
+    res.send(JSON.stringify(resultados));
+});
+
 // routing cursos de matematicas
 app.get('/api/cursos/matematicas', (req, res) => {
     res.send(JSON.stringify(infoCursos.matematicas))
